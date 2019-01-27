1efed424501799c083e1ea19031db2e52c7b6598
Hack Reactor Student <student@hackreactor.com>
2019-01-27 17:15:20 -0500

    Dynamicly render genre selection in client

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -11,36 +11,37 @@ class Search extends React.Component {
   getGenres() {
     axios.get('http://127.0.0.1:3000/genres')
     .then((result)=>{
-      this.genres = result.data.genres;
+      //this.genres = result.data.genres;
+      this.setState({genres : result.data.genres});
     })
     .catch((err)=>{console.log(err)});
     //make an axios request in this component to get the list of genres from your endpoint GET GENRES
   console.log(this.genres);
-  this.render();
+  //this.render();
   }
 
 
   render() {
+    this.getGenres();
     return (
       <div className="search">
         <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
         <br/><br/>
         
-        <div>sdlfhdjasjjjjjjfhasdlskadbvljdsfhisaufhlsdkjfl</div>
         {/* Make the select options dynamic from genres !!! */}
         {/* How can you tell which option has been selected from here? */}
 
         <select id='genresList'>
-          <option value="theway">The Way</option>
+          {/*<option value="theway">The Way</option>
           <option value="thisway">This Way</option>
           <option value="thatway">That Way</option>
-          <option value="myway">My Way</option>
+          <option value="myway">My Way</option>*/}
+          {this.state.genres.map(function(style){return <option>{style.name}</option>})}
         </select>
-          <div>ss{this.genres}aa</div>
         <script></script>
         <br/><br/>
 
-        <button onClick={()=>{this.getGenres()}}>___</button>
+        <button onClick={()=>{this.getGenres()}}>Search</button>
 
       </div>
     );
